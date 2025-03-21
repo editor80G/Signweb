91107638ff99f3a7b320399c4c03dccb4ca34193
Yuriy Grebennykov <yuri.grebennikov@gmail.com>
2025-03-21 15:45:31 +0200

    Countries has been added to the registration form

--- client/src/i18n/getTranslations.js
+++ client/src/i18n/getTranslations.js
@@ -1,6 +1,35 @@
 import { translations } from '../i18n/translations';
 
-
 export const getTranslation = (key, language = 'en') => {
-    return translations[language][key] || key;
+    const keys = Array.isArray(key) ? key : key.split('.'); // Support both arrays and dot-separated strings
+    let translation = translations[language]; // Retrieve the translation object
+
+    for (const k of keys) {
+        if (translation[k] === undefined) {
+            return key; // Return the key itself if translation is missing
+        }
+        translation = translation[k];
+    }
+
+    return translation;
 };
+
+// export const getTranslation = (key, language = 'en') => {
+//     return translations[language][key] || key;
+// };
+
+
+
+// export function GET_TRANSLATION(key, language) {
+//     const keys = key.split('.'); // Split the key by dots
+//     let translation = translations[language]; // Retrieve the translation object
+
+//     for (const k of keys) {
+//         if (translation[k] === undefined) {
+//             return key; // Return the key itself if translation is missing
+//         }
+//         translation = translation[k];
+//     }
+
+//     return translation;
+// }
